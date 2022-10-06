8f401bfcdbdaf793be39c9dd67115939ced14b79
tamiresfari <110099816+tamiresfari@users.noreply.github.com>
2022-10-06 12:37:54 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -16,6 +16,22 @@ document.write("feito por " + nome)
 
 
 
+function minhaTabuada(){
+for(let i = 1; i <= 9; i++){
+document.write(" tabuada do " +i + " <br> ");
+    for(let j = 1; j <=10; j++){
+document.write(i + " x " + " = "(j*j)+ " <br> ");
+ }
+document.write("<br>")
+}
+
+
+ function quadrado(){
+     for(let i = 16; i <= 20; i++){
+         document.write(" 0 quadrado de " + i + " é " + (i*i)+ "<br>")
+     }
+
+
 
 
 }
