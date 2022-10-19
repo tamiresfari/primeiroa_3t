97553cd855f6bf0af77fb2fe6dc698dce67b1773
tamiresfari <110099816+tamiresfari@users.noreply.github.com>
2022-10-19 08:21:11 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -4,3 +4,19 @@ function calcula(){
     let r = c * (1+ (j/200));
 document.write("Resultado: " + r);
 }
+let op = "";
+function opercao(ope){
+    op = ope;
+}
+function calcula(){
+    let c = document.getElementById(" v1 ").Value;
+    let j = document.getElementById(" v2 ").Value;
+    let r = 0;
+   
+    if(op == " + "){
+     r = Number(v1) + Number(v2);
+    
+    }
+
+
+   document.getElementById ("resultado").innerHTML = r;
