b57deee7b0531e7b7b09a52b84948a75d252c687
tamiresfari <110099816+tamiresfari@users.noreply.github.com>
2022-11-09 08:19:44 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -60,13 +60,17 @@ if (!Number(t)) {
     return
 }
 let r = c;
+let texto = "";
 for (let m = 1; m <= t; m++) {
     r = c * (1 + (j / 100));
     c = r;
-    document.write("Mes " + m + " - valor: " + moeda(r) + "<br>");
+    texto +=  m + " : " + moeda(r) + "<br>"
+   // document.write("Mes " + m + " - valor: " + moeda(r) + "<br>");
 
 }
-document.write("resultado: " + moeda(r));
+document.getElementById("listameses").innerHTML = texto;
+document.getElementById("total").innerHTML = moeda(r);
+//document.write("resultado: " + moeda(r));
 }
 
 let op = "";
@@ -95,3 +99,4 @@ function calcule() {
     document.getElementById("resultado").innerHTML = r;
 
 }
+
